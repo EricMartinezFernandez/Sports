bc33fdbe614727c06a510e39954989f2e20b9326
armasjose <armasjose1980@gmail.com>
2019-01-30 21:18:23 +0100

    Nueva escena para ver el recorrido, celda editable y clase tableViewCell

---
+++ AppSports/AppSports/TableViewCellActividad.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellActividad.swift
+//  AppSports
+//
+//  Created by  on 30/1/19.
+//  Copyright © 2019 EricJose. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellActividad: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
