9b6b306d97b685d2d9853de2990f48468afb8543
armasjose <armasjose1980@gmail.com>
2019-01-30 19:14:41 +0100

    Actualizadas las actividades
    Location manager activity y coordenadas printadas

--- AppSports/AppSports/ViewControllerCiclismo.swift
+++ AppSports/AppSports/ViewControllerCiclismo.swift
@@ -30,6 +30,7 @@ class ViewControllerCiclismo: UIViewController, CLLocationManagerDelegate{
             
             locationManager.desiredAccuracy = 1.0
             locationManager.delegate = self
+            locationManager.activityType = .fitness
             locationManager.startUpdatingLocation()
             
         } else {
@@ -42,6 +43,8 @@ class ViewControllerCiclismo: UIViewController, CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002))
         self.etiquetaMap.setRegion(region, animated: true)
+        
+        print(locations[0].coordinate.longitude)
         
     }
     
